eb0900fce8833dfa138d8f7789b62924ad8e2b1b
Tiago Silva <tiagosilva0512@gmail.com>
2022-03-17 15:46:13 -0300

    Validacao Sintatica

--- src/main/java/com/tiago/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/tiago/cursomc/resources/CategoriaResource.java
@@ -4,7 +4,8 @@ package com.tiago.cursomc.resources;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -37,16 +38,16 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
-		
-		 obj = service.insert(obj);		
-		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();	 
-		 return ResponseEntity.created(uri).build();
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO) {
+		Categoria obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();	 
+		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj , @PathVariable Integer id) {
-		
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDTO , @PathVariable Integer id) {
+		Categoria obj = service.fromDTO(objDTO);
 		obj.setId(id);
 		obj = service.update(obj);
 		
--- src/main/java/com/tiago/cursomc/services/CategoriaService.java
+++ src/main/java/com/tiago/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.tiago.cursomc.domain.Categoria;
+import com.tiago.cursomc.dto.CategoriaDTO;
 import com.tiago.cursomc.repositories.CategoriaRepository;
 import com.tiago.cursomc.services.exceptions.DataIntegrityException;
 import com.tiago.cursomc.services.exceptions.ObjectNotFoundException;
@@ -58,6 +59,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction) , orderBy);
 		
 		return repo.findAll(pageRequest);	
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO){
+		return new Categoria(objDTO.getId(),objDTO.getNome());		
 	}
 	
 }
